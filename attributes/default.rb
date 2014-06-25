f1b4b5bea490ddc2b14b0ca2ad87619b5cfb2488
Ryan Schlesinger <ryan@instanceinc.com>
2014-06-25 13:19:48 -0700

    Specify default gem version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,8 @@
-node.default[:preflight][:login_user] = case node[:platform]
+default[:preflight][:login_user] = case node[:platform]
                                 when 'ubuntu'
                                   'ubuntu'
                                 when 'amazon'
                                   'ec2-user'
                                 end
+
+default[:preflight][:gem_version] = '1.1.0'
